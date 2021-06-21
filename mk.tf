36c29e5d0d7d2acbb5357fe679047746ccf742d4
Milind Kejriwal <milindkejriwal@Milinds-MacBook-Air.local>
2021-06-22 04:34:02 +0530

    removed docker creation

--- mk.tf
+++ mk.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-    }
-
      aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
@@ -12,26 +8,9 @@ terraform {
   required_version = ">= 0.14.9"
 }
 
-provider "docker" {}
-
-
 provider "aws" {
   profile = "default"
   region  = "us-east-1"
-}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
 }
 
 resource "aws_instance" "app_server" {
